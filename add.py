12321d2e8782c595c94b7d04c329ab19203dc394
Athul K Raveendran <athulkraveendran1@gmail.com>
2019-02-23 12:46:50 +0530

    v1 addition

---
+++ add.py
@@ -0,0 +1,5 @@
+def arith(a,b):   
+    c = a+b
+    return c
+def display():
+    print(__name__)
---
+++ ari.py
@@ -0,0 +1,10 @@
+import add
+
+# n1 = int(input("Enter the first number :"))
+# n2 = int(input("Enter the sec number :"))
+# res= add.arith(n1,n2)
+# print('The result is:' ,res)
+
+
+print(__init__)
+add.display()
