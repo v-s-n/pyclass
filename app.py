09ee1fe4939713ca59532c4e8640ca58b4c20e28
Athul K Raveendran <athulkraveendran1@gmail.com>
2019-02-23 15:25:08 +0530

    v2

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "bro"
+    return render_template("index.html")
 
 @app.route("/home")
 def home():
-    return "Its my home"
+    return "<h1> Its my home </h1>"
 
 @app.route("/contact")    
 def cont():
     return "contact me @ : vsn.fun@gmail.com"
-    
+
 if(__name__ == "__main__"):
-    app.run()
+    app.run(debug =True)
